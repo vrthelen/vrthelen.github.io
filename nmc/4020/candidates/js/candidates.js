fb5685646100cf1627c2df3afc5dd06bd7d2bcba
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-09-26 21:12:23 -0400

    hf

--- nmc/4020/candidates/js/candidates.js
+++ nmc/4020/candidates/js/candidates.js
@@ -1,13 +1,8 @@
 let candidateNames = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie","Ted Cruz","Carly Fiorina","Jim Gilmore","Lindsey Graham","Bobby Jindal","John Kasich","Lawrence Lessig","Martin O'Malley","George Pataki","Rand Paul","Rick Perry","Marco Rubio","Bernie Sanders","Rick Santorum","Donald Trump","Scott Walker","Jim Webb"]
 
-
-
-
-
-
 let candidatePix = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg","graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg","perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"]
 
 let output = document.querySelector('#alsoRan');
 for (var i = 0; i < candidateNames.length; i++) {
-    output.innerHTML += candidateNames[i]+"<br><img src=pix/"+candidatePix[i]+"><br>";
+    output.innerHTML += "<li>" + candidateNames[i] + "<br><img src= pix/" + candidatePix[i] + "></li>";
 }
