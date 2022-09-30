bd8085ea22425e660160c2e1462b9a61299a0f7f
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-09-30 17:02:16 -0400

    s

--- nmc/4020/candidates/js/candidates2.js
+++ nmc/4020/candidates/js/candidates2.js
@@ -3,9 +3,7 @@ let candidateNames = ["Jeb Bush","Ben Carson","Lincoln Chafee","Chris Christie",
 let candidatePix = ["bush.jpg","carson.jpg","chafee.jpg","christie.jpg","cruz.jpg","fiorina.jpg","gilmore.jpg","graham.jpg","jindal.jpg","kasich.jpg","lessig.jpg","omalley.jpg","pataki.jpg","paul.jpg","perry.jpg","rubio.jpg","sanders.jpg","santorum.jpg","trump.jpg","walker.jpg","webb.jpg"]
 
 let output = document.querySelector('#alsoRan');
-for (var i = 0; i < candidateNames.length; i++) {
-    output.innerHTML += "<li>" + candidateNames[i] + "<br><img src= pix/" + candidatePix[i] + "></li>";
-}
+
 
 let next = document.querySelector('#next');
 let previous = document.querySelector('#previous');
