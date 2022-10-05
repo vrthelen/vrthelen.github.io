fbec24a93ed6e825f71914b3aa8bf6e42d4699fa
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-10-05 00:22:52 -0400

    k

--- nmc/4020/basicJavascript/controllingcss.js
+++ nmc/4020/basicJavascript/controllingcss.js
@@ -3,4 +3,9 @@ let sq = document.querySelector('#square');
 function changeCSS(bg,radius) {
     sq.style.backgroundColor = bg;
     sq.style.borderRadius = radius;
+    sq.style.border = '10px dashed #333';
+    sq.style.left = '100px';
+    sq.style.top = '100px';
+    sq.style.borderColor = '#000000';
+    sq.style.alignItems='center';
 }
