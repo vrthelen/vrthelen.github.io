889777cf36549cb4862081e9162dc0c789cd40e9
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-10-09 18:18:12 -0400

    a

---
+++ nmc/4020/basicJavascript/myobject.js
@@ -0,0 +1,5 @@
+let myObject = {"Coffee" : "I drink it out of necessity",
+"Tea" : "The best drink",
+"Milk" : "Put it in things or put things in it",
+"Water" : "My favorite drink",
+"Soda" : "Not fit for human consumption honestly"}
