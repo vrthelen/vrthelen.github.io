72489c43cc5410de4c5edd128a24827683b07b97
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-10-09 18:20:24 -0400

    d

--- nmc/4020/basicJavascript/myobject.js
+++ nmc/4020/basicJavascript/myobject.js
@@ -3,3 +3,11 @@ let myObject = {"Coffee" : "I drink it out of necessity",
 "Milk" : "Put it in things or put things in it",
 "Water" : "My favorite drink",
 "Soda" : "Not fit for human consumption honestly"}
+
+function showOpinion() {
+    
+    if (myObject.hasOwnProperty(sName) == true) {
+        document.querySelector('#image').innerHTML += "<img src=pix/" + nameArray[i] + ".jpg></img>";
+        document.querySelector('#aboutme').innerHTML = classObject[sName];
+    } else document.querySelector('#aboutme').innerHTML = sName + " is not in the class";
+}
