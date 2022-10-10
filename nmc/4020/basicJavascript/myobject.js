26ee51b3249cf8a7a8ae1c4349557d79394aaf0d
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-10-09 21:13:01 -0400

    a

--- nmc/4020/basicJavascript/myobject.js
+++ nmc/4020/basicJavascript/myobject.js
@@ -4,11 +4,13 @@ let myObject = {"AlexW": "I am the oldest of two boys, two girls; two redheads, 
 "Amy": " I like mint tea and taking pictures!",
 "Andrea": "Let me think about it"}
 
+let nameArray = ["AlexW","AlexC","Allison","Amy","Andrea"];
+
 for (i = 0; i < 5; i++) {
     document.querySelector('#buttons').innerHTML += "<button onclick=showOpinion('"+i+"')>Click me for "+(i+1)+"</button>";
 }
 
 function showOpinion(num) {
-    let chosenOption = document.querySelector('#options').value;
-    document.querySelector('#aboutme').innerHTML = myObject[num];
+    let choice = nameArray[num];
+    document.querySelector('#aboutme').innerHTML = myObject[choice];
 }
