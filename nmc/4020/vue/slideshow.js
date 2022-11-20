9d64cbcbd510e6a9a0022de60a0c46e7d01bf613
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-11-20 17:47:47 -0500

    a

--- nmc/4020/vue/slideshow.js
+++ nmc/4020/vue/slideshow.js
@@ -4,19 +4,42 @@ new Vue({
     data() {
         return {
             info: null,
-            moreData: []
+            moreData: [],
+            imgMaker: [],
+            whichDawg: "",
+            selector: 0
         }
     },
     mounted: function mounted() {
-        this.showme()
+        this.showme('cocker')
+
     },
+
     methods: {
         async showme(index) {
             await axios
             .get('https://dog.ceo/api/breed/spaniel/' + index + '/images')
                 .then(response => (this.info = response.data));
             console.log(this.info.message)
-            this.moreData = this.info.message
-   }
+            this.moreData = this.info.message;
+            this.slider();
+        },
+        slider(way) {
+            if (this.selector < 0 ) {
+                this.selector = this.moreData.length;
+            }
+
+            if (this.selector > this.moreData.length) {
+                this.selector = 0;
+            }
+
+            if (way == 1) {
+                this.whichDawg = this.moreData[this.selector];
+                this.selector++;
+            } else {
+                this.whichDawg = this.moreData[this.selector];
+                this.selector--;
+            }
+        }
     }
 })
