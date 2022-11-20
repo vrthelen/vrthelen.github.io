3f177b1d5035910b94ba58abe226bcbb5320d8f1
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-11-20 17:11:56 -0500

    g

--- nmc/4020/vue/component.js
+++ nmc/4020/vue/component.js
@@ -1,16 +1,17 @@
 Vue.component('toast', {
     template:
-    '<div><button v-on:click="checkToast" >check the toast</button> <h3>The toast is {{toastState}} </h3></div>'
+    '<div><button v-on:click="checkToast" >check the toast</button> <h3>The toast is {{toastState}} </h3></div>',
+    data: {
+        toastState: "burnt"
+         },
+          methods: {
+             checkToast(){
+                this.toastState = "perfect"
+             }
+         }
 })
 
 var co = new Vue({
     el: '#co',
-    data: {
-   toastState: "burnt"
-    },
-     methods: {
-        checkToast(){
-           this.toastState = "perfect"
-        }
-    }
+    
 })
