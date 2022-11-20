bd5c5db04d86d9eaa5c8c9294738d22c7fabd49d
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-11-20 17:45:33 -0500

    a

--- nmc/4020/vue/dogApi.js
+++ nmc/4020/vue/dogApi.js
@@ -2,23 +2,21 @@ new Vue({
     el: '#app',
 
     data() {
-
         return {
-            info: null
-        
+            info: null,
+            moreData: []
         }
     },
     mounted: function mounted() {
         this.showme()
-
     },
-
     methods: {
-    async    showme(index) {
-    await    axios
-                .get('https://dog.ceo/api/breed/spaniel/list')
+        async showme(index) {
+            await axios
+            .get('https://dog.ceo/api/breed/spaniel/' + index + '/images')
                 .then(response => (this.info = response.data));
-        console.log(this.info)
+            console.log(this.info.message)
+            this.moreData = this.info.message
    }
     }
 })
