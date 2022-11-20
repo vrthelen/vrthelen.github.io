67bd831dbf0baf08f4c53a63d5279a71a1b80ce2
vrthelen <104701879+vrthelen@users.noreply.github.com>
2022-11-20 16:59:45 -0500

    d

--- nmc/4020/vue/component.js
+++ nmc/4020/vue/component.js
@@ -1,3 +1,8 @@
+Vue.component('toast', {
+    template:
+    '<button v-on:click="checkToast" >check the toast</button> <h3>The toast is {{toastState}} </h3>'
+})
+
 var co = new Vue({
     el: '#co',
     data: {
